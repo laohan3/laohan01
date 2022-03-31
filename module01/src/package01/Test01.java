3f91661757eb6224faff44f2c34405b3b353c9f2
laohan3 <884307939@qq.com>
2022-03-31 21:55:14 +0800

    使用Idea测试提交

--- module01/src/package01/Test01.java
+++ module01/src/package01/Test01.java
@@ -1,6 +1,9 @@
 package package01;
 
 public class Test01 {
+    private String name;
+    private String code;
+
     public static void main(String[] args) {
         System.out.println("success||");
     }
