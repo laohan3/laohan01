025eacaa2802897fad52e12585a483e5755940b8
laohan3 <884307939@qq.com>
2022-03-31 22:27:53 +0800

    添加了新的属性

--- module01/src/package01/Test01.java
+++ module01/src/package01/Test01.java
@@ -3,6 +3,7 @@ package package01;
 public class Test01 {
     private String name;
     private String code;
+    private String newProperty;
 
     public static void main(String[] args) {
         System.out.println("success||");
